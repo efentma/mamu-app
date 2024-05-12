0a50244c87428e3ed46977ba0c549de2cdb95feb
efent <efentm8@gmail.com>
2024-05-12 13:45:42 +0700

    Add table user

---
+++ prisma/migrations/20240512064312_table_users/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `nama` VARCHAR(200) NOT NULL,
+    `email` VARCHAR(200) NOT NULL,
+    `password` VARCHAR(200) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
