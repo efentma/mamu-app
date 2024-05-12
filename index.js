13c917b212eb966981ab5251cbac58a64568a648
efent <efentm8@gmail.com>
2024-05-12 15:34:51 +0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import productRouter from "./src/routers/productRouter.js";
 import customerRouter from "./src/routers/customerRouter.js";
+// import purchaseController from "./src/controllers/purchaseController.js";
+import userRouter from "./src/routers/userRouter.js";
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(productRouter);
 app.use(customerRouter);
+// app.use(purchaseController);
+app.use(userRouter);
 
 app.listen(process.env.APP_PORT, () =>{
     console.log('Server running..');
